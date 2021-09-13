0428572d3fa14a23ff5a1396f43958d6bd1c6705
ShaitCarrillo <65733238+ShaitCarrillo@users.noreply.github.com>
2021-09-12 22:07:49 -0500

    ajustes

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+let zoomed = null
+
 for (let item of document.getElementsByClassName('gallery-container')) {
-    item.addEventListener('click',  _ => {
+    item.addEventListener('click', _ => {
         item.classList.toggle('zoomed')
+        zoomed = item
         document.getElementsByTagName('body')[0].classList.toggle('zoomed')
     })
 }
+
+document.getElementsByTagName('body')[0], addEventListener('keyup', function (sender) {
+    if (sender.key == 'Escape') {
+        document.getElementsByTagName('body')[0].classList.remove('zoomed')
+        if(zoomed != null)
+            zoomed.classList.remove('zoomed')
+    }
+})
